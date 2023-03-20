8e1f4d81bf0fffd001d66ef200d0cb7cae365695
Bob Reilly <robert.d.reilly@saic.com>
2023-03-20 16:59:36 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Parser1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Parser1.dir/main.cpp.obj"
+  "CMakeFiles/Parser1.dir/main.cpp.obj.d"
+  "Parser1.exe"
+  "Parser1.exe.manifest"
+  "Parser1.pdb"
+  "libParser1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Parser1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
